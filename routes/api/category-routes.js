03d4eceff2ff4b7e7f0368419c781e20a7d1bb03
Delilah <ila0406@gmail.com>
2022-03-28 14:17:55 -0600

    testing router.get in all three api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,48 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+    // find all categories
+    // be sure to include its associated Products
+    try {
+        const categoryData =  Category.findAll({
+            include: [{ model: Product, attributes: ['product_name'] }]
+        });
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    try {
+        const categoryData =  Category.findOne({
+            where: {id: req.params.id},
+            include: [{ model: Product, attributes: ['category_id'] }]
+        });
+    
+        if (!categoryData) {
+            res.status(404).json({ message: 'No categories with this id!' });
+            return;
+        }
+    
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+    // create a new category
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+    // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+    // delete a category by its `id` value
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,25 +4,48 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+    // find all tags
+    // be sure to include its associated Product data
+    try {
+        const tagData =  Tag.findAll({
+            include: [{ model: Product }]
+        });
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(500).json(err);
+     }
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
+    try {
+        const tagData =  Tag.findOne({
+            where: {id: req.params.id},
+            include: [{ model: Product }]
+        });
+    
+        if (!tagData) {
+            res.status(404).json({ message: 'No Tags with this id!' });
+            return;
+        }
+    
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+    // create a new tag
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+    // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+    // delete on tag by its `id` value
 });
 
 module.exports = router;
